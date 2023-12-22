a1a5e9a789986dc7e8dc22ae467c95308704361d
João Vitor Tomaz de Lima <20211170150045@ifrn.local>
2023-12-22 14:29:05 -0300

    Gasolina OK

---
+++ Hill Climb Racing (Trabalho)/Assets/Scripts/ControladorGasolina.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControladorGasolina : MonoBehaviour
+{
+    public static ControladorGasolina instance;
+
+    [SerializeField] private Image imageGasolina;
+    [SerializeField, Range(0.1f, 5f)] private float gasolinaGastaVelocidade = 1f;
+    [SerializeField] private float gasolinaMaxima = 100f;
+    [SerializeField] private Gradient gradientGasolina;
+
+    private float combustivelAtual;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    
+    void Start()
+    {
+        combustivelAtual = gasolinaMaxima;
+        UpdateUI();
+    }
+    
+    void Update()
+    {
+        combustivelAtual -= Time.deltaTime * gasolinaGastaVelocidade;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        imageGasolina.fillAmount = (combustivelAtual / gasolinaMaxima);
+        imageGasolina.color = gradientGasolina.Evaluate(imageGasolina.fillAmount);
+    }
+}
